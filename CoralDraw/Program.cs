72280f6f12ce8e37de3a5d76f545e4d09ebdf6c9
Stack <stack@uni-dubna.ru>
2021-09-05 22:39:24 +0300

    Init

---
+++ CoralDraw/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoralDraw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
